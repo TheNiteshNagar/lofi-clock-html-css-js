5e03b50d770462f39bad5b4fd33405a3d001cde1
Nitesh Nagar <159615163+xitesh@users.noreply.github.com>
2026-01-17 16:57:58 +0530

    Add digital clock and audio play functionality
    - Implemented a digital clock that updates every second.
    - Added an audio play feature triggered by clicking the '.uwu' element.
    - Styled the page with a background image and responsive design for different screen sizes.

---
+++ script.js
@@ -0,0 +1,40 @@
+document.querySelector('.uwu').addEventListener('click', () => {
+  const uwuPlay = new Audio('./public/audio-uwu.mp3')
+  uwuPlay.currentTime = 0;
+  uwuPlay.play();
+})
+function getTime() {
+
+const now = new Date();
+
+const hourNow = now.getHours();
+const minuteNow = now.getMinutes();
+const secondNow = now.getSeconds();
+
+const firstHour = document.querySelector('.first-hour')
+const secondHour = document.querySelector('.second-hour')
+
+const firstMinute = document.querySelector('.first-minute')
+const secondMinute = document.querySelector('.second-minute')
+
+const firstSecond = document.querySelector('.first-second')
+const secondSecond = document.querySelector('.second-second')
+
+
+  secondHour.textContent = hourNow;
+  firstHour.textContent = hourNow < 10 ? '0' : '';
+
+  secondMinute.textContent = minuteNow;
+  firstMinute.textContent = minuteNow < 10 ? '0' : '';
+
+  secondSecond.textContent = secondNow;
+  firstSecond.textContent = secondNow < 10 ? '0' : '';
+}
+
+
+getTime();
+
+setInterval(() => {
+  getTime()
+}, 1000);
+
